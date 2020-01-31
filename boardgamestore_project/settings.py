2407efab0388e618de88a278fa1923828fefa7f0
Ubuntu <ubuntu@ip-172-31-85-201.ec2.internal>
2020-01-31 05:50:43 +0000

    added static folder

--- boardgamestore_project/settings.py
+++ boardgamestore_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'boardgamestore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# enable static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 UPLOADCARE_PUBLIC_KEY = os.environ['UPLOADCARE_PUBLIC_KEY']
 UPLOADCARE_SECRET_KEY = os.environ['UPLOADCARE_SECRET_KEY']
