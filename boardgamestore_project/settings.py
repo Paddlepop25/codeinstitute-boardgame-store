720824f97dac80919d335032191e9cf6aceafebf
Ubuntu <ubuntu@ip-172-31-85-201.ec2.internal>
2020-01-31 07:34:05 +0000

    added user login function

--- boardgamestore_project/settings.py
+++ boardgamestore_project/settings.py
@@ -147,3 +147,5 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
 
 )
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
--- boardgamestore_project/urls.py
+++ boardgamestore_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import index as accounts_index, logout
+from accounts.views import index as accounts_index, logout, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,6 +23,6 @@ urlpatterns = [
     path('catalogue/', include('catalogue.urls')),
     path('user/', accounts_index, name='user_index'),
     path('user/logout', logout, name='logout'),
-    # path('user/login', login, name='login')
+    path('user/login', login, name='login')
 
 ]
