0bec99b903423ee749c287da8abc6d08e3c3c7b8
Ubuntu <ubuntu@ip-172-31-85-201.ec2.internal>
2020-02-03 11:34:38 +0000

    made blank possible for create game boxes

--- catalogue/models.py
+++ catalogue/models.py
@@ -5,9 +5,9 @@ from pyuploadcare.dj.models import ImageField
 class Game(models.Model):
     name = models.CharField(blank=False, max_length=255)
     description = models.TextField(blank=False)
-    inside_box = models.TextField(blank=False)
+    inside_box = models.TextField(blank=True)
     available = models.BooleanField(blank=True)
-    stock_left = models.IntegerField(default=0, blank=False)
+    stock_left = models.IntegerField(default=0, blank=True)
     price = models.DecimalField(default=0, max_digits=5, decimal_places=2, blank=False)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     # discount = models.ManyToManyField('Discount')
