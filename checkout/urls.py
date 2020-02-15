5eb2e92b1d7a0c0296e0b4e6ca0c99dcc56c1425
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-02-15 09:00:34 +0000

    created skeleton stripe payment

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import checkout, checkout_success, checkout_cancelled
+# from .views import checkout, checkout_success, checkout_cancelled
+from .views import donate, charge
 
 urlpatterns = [
-    path('', checkout, name='checkout'),
-    path('success', checkout_success),
-    path('cancelled', checkout_cancelled)    
+    path('', donate, name='donate'),
+    path('charge', charge, name='charge')
+    # path('', checkout, name='checkout'),
+    # path('success', checkout_success),
+    # path('cancelled', checkout_cancelled)    
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,54 +1,74 @@
 from django.shortcuts import render, reverse, HttpResponse, get_object_or_404
+from .forms import OrderForm, PaymentForm
+
 
 #import settings so that we can access the public stripe key
 from django.conf import settings
-import stripe
+# import stripe
 
-from catalogue.models import Game
+# from catalogue.models import Game
 
 # Create your views here.
-def checkout(request):
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-    
-    # retrieve shopping cart
-    cart = request.session.get('shopping_cart', {})
-    
-    line_items = []
-    
-    # generate the line_items
-    for id,game in cart.items():
-        # For each item in the cart, get its details from the database
-        game_from_db = get_object_or_404(Game, pk=id)
-        line_items.append({
-            'name': game_from_db.name,
-            'amount': int(game_from_db.price*100), #convert to cents, in integer
-            'currency':'sgd',
-            'quantity':game['quantity']
-        })
-    
-    #generate the session
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=line_items,
-        success_url=request.build_absolute_uri(reverse(checkout_success)),
-        cancel_url=request.build_absolute_uri(reverse(checkout_cancelled)),
-        payment_intent_data={
-            'capture_method':'manual'
-        }
-    )
-    
-    # render the template
-    return render(request, 'checkout/checkout.template.html', {
-        'session_id':session.id,
-        'public_key': settings.STRIPE_PUBLISHABLE_KEY
+
+def donate(request):
+    return render(request, 'checkout/donate.template.html')
+
+def charge(request):
+    order_form = OrderForm()
+    payment_form = PaymentForm()
+    amount = request.GET['amount']
+    # if request.method == 'GET':
+        #show form
+    return render(request, 'checkout/charge.html', {
+        # 'publishable' : settings.STRIPE_PUBLISHABLE_KEY,
+        'order_form' : OrderForm,
+        'payment_form' : PaymentForm,
+        'amount': amount
     })
     
-def checkout_success(request):
-    request.session['shopping_cart'] = {}
-    return render(request, 'checkout/checkout_success.template.html')
+# simple method
+# def checkout(request):
+#     stripe.api_key = settings.STRIPE_SECRET_KEY
     
-def checkout_cancelled(request):
-    return HttpResponse("Checkout cancelled")
+#     # retrieve shopping cart
+#     cart = request.session.get('shopping_cart', {})
+    
+#     line_items = []
+    
+#     # generate the line_items
+#     for id,game in cart.items():
+#         # For each item in the cart, get its details from the database
+#         game_from_db = get_object_or_404(Game, pk=id)
+#         line_items.append({
+#             'name': game_from_db.name,
+#             'amount': int(game_from_db.price*100), #convert to cents, in integer
+#             'currency':'sgd',
+#             'quantity':game['quantity']
+#         })
+    
+#     #generate the session
+#     session = stripe.checkout.Session.create(
+#         payment_method_types=['card'],
+#         line_items=line_items,
+#         success_url=request.build_absolute_uri(reverse(checkout_success)),
+#         cancel_url=request.build_absolute_uri(reverse(checkout_cancelled)),
+#         payment_intent_data={
+#             'capture_method':'manual'
+#         }
+#     )
+    
+#     # render the template
+#     return render(request, 'checkout/checkout.template.html', {
+#         'session_id':session.id,
+#         'public_key': settings.STRIPE_PUBLISHABLE_KEY
+#     })
+    
+# def checkout_success(request):
+#     request.session['shopping_cart'] = {}
+#     return render(request, 'checkout/checkout_success.template.html')
+    
+# def checkout_cancelled(request):
+#     return HttpResponse("Checkout cancelled")
     
     
     
