e19f270d469ed7cd78bde39b9aaead91ca1626ee
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-02-20 05:18:20 +0000

    managed to hide 'checkout' button when cart is empty

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,10 +2,11 @@
 
 let cat = document.querySelector("#meow");
 let table_head = document.querySelector('#table_head');
+let checkout_mobile = document.querySelector('#checkout_mobile');
+let checkout_tablet = document.querySelector('#checkout_tablet');
 
 if (cat) {
     table_head.style.display = "none";
+    checkout_mobile.style.display = "none";
+    checkout_tablet.style.display = "none";
 }
-
-// let checkout_form = document.querySelector(".checkout_form");
-
