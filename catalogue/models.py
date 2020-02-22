b2362cf7c29caa99fdedf363aea64139933b5df9
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-02-22 08:53:44 +0000

    added erdiagram picture

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# from django.core.validators import MinValueValidator, MaxValueValidator
 from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
@@ -7,9 +6,6 @@ class Game(models.Model):
     name = models.CharField(blank=False, max_length=21)
     description = models.TextField(blank=False)
     inside_box = models.TextField(blank=False)
-    # available = models.BooleanField(blank=True)
-    # stock_left = models.IntegerField(default=0, blank=True, validators=[MinValueValidator(0), MaxValueValidator(99)])
-    # stock_left = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     price = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     image = ImageField(null=True, blank=True)
