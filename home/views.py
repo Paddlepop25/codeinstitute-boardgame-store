701986f9cab86b446a8450592ce4a78fd7b664d2
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-02-24 12:25:24 +0000

    error 505 deployment

--- home/views.py
+++ home/views.py
@@ -3,10 +3,22 @@ from catalogue.models import Game
 # Create your views here.
 
 def home(request):
-    dixit_game = Game.objects.get(name='Dixit')
-    resistance_game = Game.objects.get(name='The Resistance')
-    catan_game = Game.objects.get(name='Catan')
-    betrayal_game = Game.objects.get(name='Betrayal at House')
+    try:
+        dixit_game = Game.objects.get(name='Dixit')
+    except Game.DoesNotExist:
+        dixit_game = None
+    try:
+        resistance_game = Game.objects.get(name='The Resistance')
+    except Game.DoesNotExist:
+        resistance_game = None
+    try:
+        catan_game = Game.objects.get(name='Catan')
+    except Game.DoesNotExist:
+        catan_game = None
+    try:
+        betrayal_game = Game.objects.get(name='Betrayal at House')
+    except Game.DoesNotExist:
+        betrayal_game = None
     return render(request, 'home/index.template.html', {
         'dixit':dixit_game,
         'resistance':resistance_game,
